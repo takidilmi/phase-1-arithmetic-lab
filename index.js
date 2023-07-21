c87a520c03fc0ee953791dc4d2519880af8e6fcd
takidilmi <angeldilmi@gmail.com>
2023-07-21 14:40:46 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 100);
+let num3 = 24;
+let num4 = 10;
+let mod = num3 % num4;
+let set = [14, 10, 20, 3, 17];
+let max = Math.max(...set);
